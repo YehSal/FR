de64dbd6f025fad85c6e3022c774b0da7e0ffdde
Yehia Saleh <yehia.saleh@yale.edu>
2017-01-08 19:37:19 +0200

    Scraping: Absolute url paths for profiles, cleaned up all newlines in strings

--- data/imdb/imdb/pipelines.py
+++ data/imdb/imdb/pipelines.py
@@ -6,6 +6,6 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-class FantasyRealityPipeline(object):
+class ImdbPipeline(object):
     def process_item(self, item, spider):
         return item
--- data/imdb/imdb/spiders/imdb_spider.py
+++ data/imdb/imdb/spiders/imdb_spider.py
@@ -1,8 +1,17 @@
 import scrapy
 import pdb
 
+
+# To construct full absolute url paths
+try:
+    from urlparse import urljoin  # Python2
+except ImportError:
+    from urllib.parse import urljoin  # Python3
+
+
 class ImdbSpider(scrapy.Spider):
     name = "imdb"
+    homepage = 'http://www.imdb.com'
     def start_requests(self):
         urls = [
         'http://www.imdb.com/title/tt0120596/fullcredits',
@@ -13,32 +22,45 @@ class ImdbSpider(scrapy.Spider):
     def parse(self, response):
         for actor in response.css('tr.odd'):
             char = actor.css('td.character a::text').extract_first()
+            link = urljoin(
+                "http://www.imdb.com",
+            ' '.join(actor.css('td.primary_photo a::attr(href)').extract_first().split()))
+
             if char == None:
                 yield {
-                    'real_name': actor.css('td.itemprop a span.itemprop::text').extract_first().strip(),
-                    'character_name': actor.css('td.character div::text').extract_first().strip(),
-                    'imdb_link': actor.css('td.primary_photo a::attr(href)').extract_first(),
+                    'real_name':
+                    ' '.join(actor.css('td.itemprop a span.itemprop::text').extract_first().split()),
+                    'character_name':
+                    ' '.join(actor.css('td.character div::text').extract_first().split()),
+                    'imdb_link': link,
                 }
             else:
                 yield {
-                    'real_name': actor.css('td.itemprop a span.itemprop::text').extract_first().strip(),
-                    'character_name': actor.css('td.character a::text').extract_first().strip(),
-                    'imdb_link': actor.css('td.primary_photo a::attr(href)').extract_first(),
+                    'real_name':
+                    ' '.join(actor.css('td.itemprop a span.itemprop::text').extract_first().split()),
+                    'character_name':
+                    ' '.join(actor.css('td.character a::text').extract_first().split()),
+                    'imdb_link': link,
                 }
 
         for actor in response.css('tr.even'):
             char = actor.css('td.character a::text').extract_first()
             if char == None:
+                link = response.urljoin(' '.join(actor.css('td.primary_photo a::attr(href)').extract_first().split()))
                 yield {
-                    'real_name': actor.css('td.itemprop a span.itemprop::text').extract_first().strip(),
-                    'character_name': actor.css('td.character div::text').extract_first().strip(),
-                    'imdb_link': actor.css('td.primary_photo a::attr(href)').extract_first(),
+                    'real_name':
+                    ' '.join(actor.css('td.itemprop a span.itemprop::text').extract_first().split()),
+                    'character_name':
+                    ' '.join(actor.css('td.character div::text').extract_first().split()),
+                    'imdb_link': link,
                 }
             else:
                 yield {
-                    'real_name': actor.css('td.itemprop a span.itemprop::text').extract_first().strip(),
-                    'character_name': actor.css('td.character a::text').extract_first().strip(),
-                    'imdb_link': actor.css('td.primary_photo a::attr(href)').extract_first(),
+                    'real_name':
+                    ' '.join(actor.css('td.itemprop a span.itemprop::text').extract_first().split()),
+                    'character_name':
+                    ' '.join(actor.css('td.character a::text').extract_first().split()),
+                    'imdb_link': link,
                 }
 
 
